cf135d6a3ae7ce78245df015ee78b09bc49c30b7
elf Pavlik <elf-pavlik@hackers4peace.net>
2016-12-17 12:58:51 -0600

    Mexico only seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,26 +2,25 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 # -- Spree
-unless Spree::Country.find_by_name 'Australia'
+unless Spree::Country.find_by_name 'Mexico'
   puts "[db:seed] Seeding Spree"
-  Spree::Core::Engine.load_seed if defined?(Spree::Core)
+  #Spree::Core::Engine.load_seed if defined?(Spree::Core)
+  Spree::Country.create!({"name"=>"Mexico", "iso3"=>"MEX", "iso"=>"MX", "iso_name"=>"MEXICO", "numcode"=>"484"}, :without_protection => true)
+  zone = Spree::Zone.create!(:name => "Mexico", :description => "Mexico")
+  zone.zone_members.create!(:zoneable => Spree::Country.find_by_name!("Mexico"))
+  Spree::Role.create!(:name => "admin")
+  Spree::Role.create!(:name => "user")
+
   Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
 end
 
 # -- States
-unless Spree::State.find_by_name 'Victoria'
-  country = Spree::Country.find_by_name('Australia')
+unless Spree::State.find_by_name 'CDMX'
+  country = Spree::Country.find_by_name('Mexico')
   puts "[db:seed] Seeding states"
 
   [
-   ['ACT', 'ACT'],
-   ['New South Wales', 'NSW'],
-   ['Northern Territory', 'NT'],
-   ['Queensland', 'QLD'],
-   ['South Australia', 'SA'],
-   ['Tasmania', 'Tas'],
-   ['Victoria', 'Vic'],
-   ['Western Australia', 'WA']
+   ['CDMX', 'CDMX']
   ].each do |state|
     Spree::State.create!({"name"=>state[0], "abbr"=>state[1], :country=>country}, :without_protection => true)
   end
